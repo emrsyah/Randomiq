f409a61e69bacd87963ac94676950dda7d5f958e
Emirsyah <muhammademir48@gmail.com>
2022-04-16 22:34:10 +0700

    change loading spinner between pages

--- src/App.js
+++ src/App.js
@@ -5,16 +5,28 @@ import { useAuth0 } from "@auth0/auth0-react";
 import { RecoilRoot } from "recoil";
 import Challenges from "./pages/Challenges";
 import { ToastContainer } from "react-toastify";
-import loading from "./assets/loading.svg";
+import logo from "./assets/randomiq-logo-top.svg";
 import Modal from "./components/Modal";
+import lottiejson from './assets/99853-spinner.json'
+import { useEffect } from "react";
+import Lottie from "lottie-web";
 
 
 function App() {
   const { isLoading } = useAuth0();
+  useEffect(()=>{
+    Lottie.loadAnimation({
+      container: document.querySelector('#lottie-container'),
+      animationData: lottiejson,
+    })
+  },[])
+
   if (isLoading)
     return (
-      <div className="flex justify-center items-center h-[100vh]">
-        <img src={loading} alt="" />
+      <div className="flex justify-center items-center h-[100vh] flex-col">
+        {/* <img src={loading} alt="" /> */}
+        <img src={logo} alt="" className="h-30" />
+        <div id="lottie-container" className="w-20 h-20"/>
       </div>
     );
 
